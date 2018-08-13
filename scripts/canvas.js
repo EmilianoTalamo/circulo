b0b45f0885241ca2754b17309c643886570697a5
EmilianoTalamo <emilianotalamo@gmail.com>
2018-08-13 17:10:03 -0300

    Nicer code

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,26 +1,31 @@
+// Variable initialization
+var fileReader = new FileReader();
 var preview = document.getElementById("preview");
 var ctx = preview.getContext("2d");
 
-function clearCanvas() {
-	preview.getContext('2d').clearRect(0, 0, preview.width, preview.height);
-	drawCircle();
-}
 
+// Get selected photo
 document.getElementById("pic").onchange = function(e) {
+	// If the file is not a picture do nothing
 	if(!document.getElementById("pic").files[0].type.match("image.*")){
 		return false;
 	}
 
+	// Clear the canvas content
 	clearCanvas();
 
+	// When the file finished loading
 	fileReader.onload = function(file) {
 		var img = new Image();
 		
 		img.onload = function() {
 			drawCircle();
 			var posX, posY, drawW, drawH;
-			var ogheight = img.naturalHeight;
-			var ogwidth = img.naturalWidth;
+			var ogheight = img.naturalHeight; // Get file height
+			var ogwidth = img.naturalWidth; // Get file width
+
+			// Aspect ratio correction
+
 			// if uploaded pic is wide
 			if(ogwidth > ogheight) {
 				drawW = 400 * Math.abs(ogwidth / ogheight);
@@ -28,6 +33,7 @@ document.getElementById("pic").onchange = function(e) {
 				posY = 0;
 				posX = (400 - drawW) / 2;
 			}
+
 			// if uploaded pic is tall
 			else if(ogwidth < ogheight) {
 				drawW = 400;
@@ -35,37 +41,56 @@ document.getElementById("pic").onchange = function(e) {
 				posY = (400 - drawH) / 2;
 				posX = 0;
 			}
+
 			// if uploaded pic is 1:1
-			else {
-				
+			else {	
 				posX = 0;
 				posY = 0;
 				drawW = 400;
 				drawH = 400;
 			}
+
+			// Position the corrected image to the canvas
 			ctx.drawImage(img,posX,posY,drawW,drawH);
+			drawCircle();
 		};
     img.src = event.target.result;
     };
 		fileReader.readAsDataURL(e.target.files[0]);
 };
 
-// Circulo
 
+// This function will draw the circle with the desired settings
 function drawCircle() {
 	ctx.clearRect(0, 0, ctx.width, ctx.height);
+
+	// Draw on the top layer
 	ctx.globalCompositeOperation = 'source-over';
+
+	// This sets the starting drawing point on the top center angle of the circle
 	var offset = 300;
+
+	// Initial position of the angles
 	var angle1 = 0, angle2 = 0;
+
+	// This loop draws the segments of the circle based on the quantity chosen by the user
 	for(let i=1 ; i<=colorqt ; i++) {
 		ctx.beginPath();
-		ctx.lineWidth = "20";
-		ctx.strokeStyle=colorArr[i-1];
+		ctx.lineWidth = "20"; // Width of the stoke (constant across all strokes)
+		ctx.strokeStyle=colorArr[i-1]; // Color of the current stroke
 		angle1 = angle2;
 		angle2 = 360/colorqt * i;
 		ctx.arc(200,200,190,(Math.PI/180)*angle1+offset,(Math.PI/180)*angle2+offset,false);
 		ctx.stroke();
 	}
+
+	// Draw on the back layer again
 	ctx.globalCompositeOperation = 'destination-over';
 }
 
+
+// This function clears the canvas entirely, and draws a new circle
+function clearCanvas() {
+	preview.getContext('2d').clearRect(0, 0, preview.width, preview.height);
+	drawCircle();
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,114 @@
+// Variable initialization
+var colorqt;
+
+
+// Create color array and fill it with random colors
 var colorArr = [];
 for(let i=0 ; i<8 ; i++) {
 	colorArr[i] = generateRandomHex();
 }
-var colorqt;
-var fileReader = new FileReader();
 
+
+// Stuff to do when web loads
+window.onload = function() {
+
+	// Get position of quantity slider
+	updateqtnum();
+
+	// Draw canvas circle
+	drawCircle();
+
+	// Load presets
+	document.getElementById("presetsInput").appendChild(document.createElement("option")).innerHTML = "Personalizado";
+	for(let i of presets) {
+		document.getElementById("presetsInput").appendChild(document.createElement("option")).innerHTML = i.name;
+	}
+};
+
+
+// Quantity slider event listener
+document.querySelector("#colorqt").addEventListener("change", function() {
+	updateqtnum(); // Update number and colors
+	drawCircle(); // Draw canvas circle
+	presetToCustom(); // Change preset to custom
+});
+
+
+// Quantity slider buttons
+// Button (-)
+document.getElementById("colorqtMinus").addEventListener("click", function() {
+	document.getElementById("colorqt").value--;
+	updateqtnum();
+	presetToCustom();
+});
+// Button (+)
+document.getElementById("colorqtPlus").addEventListener("click", function() {
+	document.getElementById("colorqt").value++;
+	updateqtnum();
+	presetToCustom();
+});
+
+
+// Color change event listener
+document.querySelector("#colors").addEventListener("change", function() {
+	// Get all colors and write them to the array
+	let currentColors = document.getElementsByClassName("colorPicker");
+	for(let i=0 ; i<currentColors.length ; i++) {
+		colorArr[i] = currentColors[i].value;
+	}
+	drawCircle(); // Draw canvas circle
+	presetToCustom(); // Change preset to custom
+});
+
+
+// Preset chosen
+document.querySelector("#presets").addEventListener("change", function() {
+	// Get selected preset
+	let index = document.getElementById("presetsInput").selectedIndex;
+
+	// If custom, do nothing
+	if(index == 0) {
+		return false;
+	}
+
+	// Change index number to match array's
+	index--;
+
+	// Update color quantity
+	document.getElementById("colorqt").value = presets[index].qt;
+	updateqtnum();
+
+	// Update colors
+	var colors = document.getElementsByClassName("colorPicker");
+	for(let i=0 ; i<presets[index].qt ; i++) {
+		colors[i].value = presets[index].colors[i];
+		colorArr[i] = presets[index].colors[i];
+	}
+
+	// Finally, update canvas
+	drawCircle();
+});
+
+
+// Save button
+document.getElementById("dl").addEventListener("click", function() {
+	this.href = document.getElementById("preview").toDataURL('image/jpeg'); // Catch canvas content as jpeg
+	this.download = "avatar.jpg"; // File name
+});
+
+
+// Functions
+
+// This function updates the number of the color quantity slider and calls the colors updater function, it also draws the canvas again
 function updateqtnum() {
 	document.querySelector("#colorqtnum").innerHTML = document.querySelector("#colorqt").value;
 	colorqt = document.querySelector("#colorqt").value;
-	showColors();
-	drawCircle();
+	showColors(); // Color updater function
+	drawCircle(); // Draw canvas circle
 }
 
+
+// Colors updater function. Gets the value of the slider and updates the quantity of colors to be shown
 function showColors() {
 	let colorqt = document.querySelector("#colorqt").value;
 	let colors = document.querySelector("#colors > div");
@@ -26,6 +123,8 @@ function showColors() {
 	}
 }
 
+
+// Random color generator (hex)
 function generateRandomHex() {
 	var letters = '0123456789ABCDEF'.split('');
 	var color = '#';
@@ -35,75 +134,8 @@ function generateRandomHex() {
 	return color;
 }
 
-// Stuff to change when loading webpage
-window.onload = function() {
-	updateqtnum();
-	drawCircle();
 
-	document.getElementById("presetsInput").appendChild(document.createElement("option")).innerHTML = "Personalizado";
-	for(let i of presets) {
-		document.getElementById("presetsInput").appendChild(document.createElement("option")).innerHTML = i.name;
-	}
-};
-
-// Update when slider changes
-document.querySelector("#colorqt").addEventListener("change", function() {
-	updateqtnum();
-	drawCircle();
-	presetToCustom();
-});
-
-// Update when color changes
-document.querySelector("#colors").addEventListener("change", function() {
-	let currentColors = document.getElementsByClassName("colorPicker");
-	for(let i=0 ; i<currentColors.length ; i++) {
-		colorArr[i] = currentColors[i].value;
-	}
-	drawCircle();
-	presetToCustom();
-});
-
+// This changes the presets dropdown to custom when triggered
 function presetToCustom() {
 	document.querySelector("#presetsInput").selectedIndex = 0;
 }
-
-// Update when preset is selected
-document.querySelector("#presets").addEventListener("change", function() {
-	// Get selected preset
-	let index = document.getElementById("presetsInput").selectedIndex;
-	// If custom, do nothing
-	if(index == 0) {
-		return false;
-	}
-	// Change index number to match array's
-	index--;
-	// Update color quantity
-	document.getElementById("colorqt").value = presets[index].qt;
-	updateqtnum();
-	// Update colors
-	var colors = document.getElementsByClassName("colorPicker");
-	for(let i=0 ; i<presets[index].qt ; i++) {
-		colors[i].value = presets[index].colors[i];
-		colorArr[i] = presets[index].colors[i];
-	}
-	// Finally, update canvas
-	drawCircle();
-});
-
-// colorqt + and -
-document.getElementById("colorqtMinus").addEventListener("click", function() {
-	document.getElementById("colorqt").value--;
-	updateqtnum();
-	presetToCustom();
-});
-document.getElementById("colorqtPlus").addEventListener("click", function() {
-	document.getElementById("colorqt").value++;
-	updateqtnum();
-	presetToCustom();
-});
-
-// Save btn
-document.getElementById("dl").addEventListener("click", function() {
-	this.href = document.getElementById("preview").toDataURL('image/jpeg');
-	this.download = "avatar.jpg";
-});
