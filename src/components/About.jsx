f38214a5d822ccbe7ee1193f0bad27e3a7b3e40b
emilianotalamo <emilianotalamo@gmail.com>
2020-04-19 00:41:07 -0300

    Fixed linter warnings

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const About = () => <a href="http://emilianotalamo.github.io/" target="_blank" id="about">Emit</a>
+const About = () => <a href="http://emilianotalamo.github.io/" target="_blank" id="about" rel="noopener noreferrer">Emit</a>
 
 export default About
